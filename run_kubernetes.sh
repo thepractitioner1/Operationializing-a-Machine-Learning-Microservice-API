179c4451d3c8ccb0b887e7c07ab1251b0b02b88f
mokerentie <mokerentie@paga.com>
2020-07-28 15:12:21 +0100

    Updated kubernetes configuration

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="thepract/myflaskapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run myflaskapp\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=myflaskapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskapp 8000:80
