f75298441a22b46ad47331ffdeb942248343c861
mokerentie <mokerentie@paga.com>
2020-07-28 14:57:02 +0100

    Modified Upload docker image script

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="thepract/myflaskapp"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag myflaskapp $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
